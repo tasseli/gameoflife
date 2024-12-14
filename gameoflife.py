22fa20e3eb83c14c874ec55fe6f7c5d21bb9f382
Mikael Merenparras <tassel@iki.fi>
2024-12-14 23:43:57 +0200

    Drawing a matrix

---
+++ gameoflife.py
@@ -0,0 +1,8 @@
+# Creates a list containing 5 lists, each of 8 items, all set to 0
+w, h = 8, 5
+matrix = [[0 for x in range(w)] for y in range(h)] 
+matrix[1][2] = 1
+for i in range (0,h):
+    for j in range (0,w):
+        print(matrix[i][j], end="")
+    print()
