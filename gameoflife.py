9ad1e7f20edac82861be5e24fe4af70faed70a9d
Mikael Merenparras <tassel@iki.fi>
2024-12-18 00:07:29 +0200

    Added a shape that creates interesting development for next round

--- gameoflife.py
+++ gameoflife.py
@@ -2,6 +2,10 @@
 w, h = 8, 5
 matrix = [[0 for x in range(w)] for y in range(h)] 
 matrix[1][2] = 1
+matrix[2][2] = 1
+matrix[2][3] = 1
+matrix[0][3] = 1
+
 for i in range (0,h):
     for j in range (0,w):
         print(matrix[i][j], end="")
